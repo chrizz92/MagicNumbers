23c72c1857de477a4d69fb1706f554cac20971f6
Christian Schadler <c.schadler@HTBLALeonding.onmicrosoft.com>
2019-12-09 23:48:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MagicNumber.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, digitOne, digitTwo, digitThree;
+            
+            for(int i = 100; i < 1000; i++)
+            {
+                number = i;
+                digitOne = number / 100;
+                number -= digitOne*100;
+                digitTwo = number / 10;
+                number -= digitTwo*10;
+                digitThree = number;
+                number = (digitOne * digitOne * digitOne) + (digitTwo * digitTwo * digitTwo) + (digitThree * digitThree * digitThree);
+
+
+                if (i == number)
+                {
+                    Console.WriteLine("Eine Magische Zahl lautet {0} da {1}^3+{2}^3+{3}^3={0}",i,digitOne,digitTwo,digitThree);
+                }
+
+                
+
+            }
+        }
+    }
+}
